d4356a611d41a5d85ae8c178839a3917663c8855
dog2023g <139338745+dog2023g@users.noreply.github.com>
2024-04-15 02:37:46 +0500

    Добавление файла для урока 1
    Добавлен массив с тестами, массив с переводами

---
+++ Main/js/translations1.js
@@ -0,0 +1,242 @@
+var toMain='https://dog2023g.github.io/GuideToOnlineCourses/Main/index.html';
+var lessons='https://dog2023g.github.io/GuideToOnlineCourses/Main/index.html';
+var aboutUs=window.location.href.split(/[#?]+/)[0];
+var openedu='https://openedu.ru/?ysclid=luha0dkctv671818426';
+var elern='https://elearn.urfu.ru/';
+var exam1='https://exam1.urfu.ru/';
+var exam2='https://exam2.urfu.ru/';
+var lesson_1='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson1.html';
+var lesson_2='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson2.html';
+var lesson_3='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson3.html';
+var lesson_4='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson4.html';
+var lesson_5='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson5.html';
+var lesson_6='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson6.html';
+var lesson_7='https://dog2023g.github.io/GuideToOnlineCourses/Main/lesson7.html';
+const tests_any_lang={ 
+    ru:[
+    {
+      question: '1. Твои данные в поле идентификатора не находятся. Что будешь делать?',
+      answers: ['Проверю корректность своих данных, а также удостоверюсь, что уже можно начинать регистрироваться',
+      'Начну истерить, что ничего не получается'],
+      correct: 1,
+    },
+    {
+        question: '2. Доступ к office.com не активировался сразу после подачи заявки. Твои действия?',
+        answers: ['Паникую, навожу суету',
+        'Пишу в техподдержку, чтобы как можно быстрее дали доступ!',
+        'Терпеливо жду 36 часов'],
+        correct: 3,
+      },
+      {
+        question: '3. Получив корпоративную почту, ты…',
+        answers: ['Запомнишь её, зафиксируешь в заметках',
+        'Тут же забудешь про неё. Зачем тебе ещё одна?',
+        'Отлично! Теперь пора приступать к следующему этапу!'],
+        correct: 1,
+      }],
+      en:[
+        {
+          question: 'Английский. 1. Твои данные в поле идентификатора не находятся. Что будешь делать?',
+          answers: ['Проверю корректность своих данных, а также удостоверюсь, что уже можно начинать регистрироваться',
+          'Начну истерить, что ничего не получается'],
+          correct: 1,
+        },
+        {
+            question: '2. Доступ к office.com не активировался сразу после подачи заявки. Твои действия?',
+            answers: ['Паникую, навожу суету',
+            'Пишу в техподдержку, чтобы как можно быстрее дали доступ!',
+            'Терпеливо жду 36 часов'],
+            correct: 3,
+          },
+          {
+            question: '3. Получив корпоративную почту, ты…',
+            answers: ['Запомнишь её, зафиксируешь в заметках',
+            'Тут же забудешь про неё. Зачем тебе ещё одна?',
+            'Отлично! Теперь пора приступать к следующему этапу!'],
+            correct: 1,
+          }],
+          ch:[
+            {
+              question: 'Китайский. 1. Твои данные в поле идентификатора не находятся. Что будешь делать?',
+              answers: ['Проверю корректность своих данных, а также удостоверюсь, что уже можно начинать регистрироваться',
+              'Начну истерить, что ничего не получается'],
+              correct: 1,
+            },
+            {
+                question: '2. Доступ к office.com не активировался сразу после подачи заявки. Твои действия?',
+                answers: ['Паникую, навожу суету',
+                'Пишу в техподдержку, чтобы как можно быстрее дали доступ!',
+                'Терпеливо жду 36 часов'],
+                correct: 3,
+              },
+              {
+                question: '3. Получив корпоративную почту, ты…',
+                answers: ['Запомнишь её, зафиксируешь в заметках',
+                'Тут же забудешь про неё. Зачем тебе ещё одна?',
+                'Отлично! Теперь пора приступать к следующему этапу!'],
+                correct: 1,
+              }],
+              ar:[
+                {
+                  question: 'Арабский. 1. Твои данные в поле идентификатора не находятся. Что будешь делать?',
+                  answers: ['Проверю корректность своих данных, а также удостоверюсь, что уже можно начинать регистрироваться',
+                  'Начну истерить, что ничего не получается'],
+                  correct: 1,
+                },
+                {
+                    question: '2. Доступ к office.com не активировался сразу после подачи заявки. Твои действия?',
+                    answers: ['Паникую, навожу суету',
+                    'Пишу в техподдержку, чтобы как можно быстрее дали доступ!',
+                    'Терпеливо жду 36 часов'],
+                    correct: 3,
+                  },
+                  {
+                    question: '3. Получив корпоративную почту, ты…',
+                    answers: ['Запомнишь её, зафиксируешь в заметках',
+                    'Тут же забудешь про неё. Зачем тебе ещё одна?',
+                    'Отлично! Теперь пора приступать к следующему этапу!'],
+                    correct: 1,
+                  }]
+    };
+
+const language={
+    "link_our_name":{ //навигационная панель
+        "ru": '<a class="navbar-brand" href="'+toMain+'"><img src="img/seal_12890965.png" alt="&nbspТюлени Свободы" width="50" height="50"></a>',
+        "en": '<a class="navbar-brand" href="'+toMain+'?lang=en&"><img src="img/seal_12890965.png" alt="&nbspRadio Seals" width="50" height="50"></a>',
+        "ch": '<a class="navbar-brand" href="'+toMain+'?lang=ch&"><img src="img/seal_12890965.png" alt="&nbsp自由印章" width="50" height="50"></a>',
+        "ar": '<a class="navbar-brand" href="'+toMain+'?lang=ar&"><img src="img/seal_12890965.png" alt="&nbspأختام الحرية" width="50" height="50"></a>'
+    },
+    "link_name":{ //навигационная панель
+        "ru": '<a class="nav-link active" aria-current="page" href="'+toMain+'">Главная</a>',
+        "en": '<a class="nav-link active" aria-current="page" href="'+toMain+'?lang=en&">Main</a>',
+        "ch": '<a class="nav-link active" aria-current="page" href="'+toMain+'?lang=ch&">首頁</a>',
+        "ar": '<a class="nav-link active" aria-current="page" href="'+toMain+'?lang=ar&">بيت</a>'
+    },
+    "link_lessons":{ //навигационная панель
+        "ru": '<a class="nav-link" href="'+lessons+'?lang=ru&#to_lessons">Уроки</a>',
+        "en": '<a class="nav-link" href="'+lessons+'?lang=en&#to_lessons">Lessons</a>',
+        "ch": '<a class="nav-link" href="'+lessons+'?lang=ch&#to_lessons">教訓</a>',
+        "ar": '<a class="nav-link" href="'+lessons+'?lang=ar&#to_lessons">دروس</a>'
+    },
+    "link_about_us":{ //навигационная панель
+        "ru": '<a class="nav-link" href="'+aboutUs+'?lang=ru&#footer">О нас</a>',
+        "en": '<a class="nav-link" href="'+aboutUs+'?lang=en&#footer">About us</a>',
+        "ch": '<a class="nav-link" href="'+aboutUs+'?lang=ch&#footer">關於我們</a>',
+        "ar": '<a class="nav-link" href="'+aboutUs+'?lang=ar&#footer">معلومات عنا</a>'
+    },
+    "search":{ //навигационная панель
+        "ru": '<input class="form-control me-2" type="search" placeholder="Например: Exam2" aria-label="Search"><button class="btn btn-outline-success" type="submit">Найти</button>',
+        "en": '',
+        "ch": '',
+        "ar": ''
+    },
+    "link_openedu":{ //футер
+        "ru": '<a class="nav-link" href="'+openedu+'">OpenEdu (Открытое образование)</a>',
+        "en": '<a class="nav-link" href="'+openedu+'">OpenEdu (Открытое образование (Open Education) platform)</a>',
+        "ch": '<a class="nav-link" href="'+openedu+'">OpenEdu</a>',
+        "ar": '<a class="nav-link" href="'+openedu+'">OpenEdu</a>'
+    },
+    "link_elearn":{ //футер
+        "ru": '<a class="nav-link" href="'+elern+'">elearn (Портал электронного обучения)</a>',
+        "en": '<a class="nav-link" href="'+elern+'">elearn (Electronic Learning Portal)</a>',
+        "ch": '<a class="nav-link" href="'+elern+'">elearn</a>',
+        "ar": '<a class="nav-link" href="'+elern+'">elearn</a>',
+    },
+    "link_exam1":{ //футер
+        "ru": '<a class="nav-link" href="'+exam1+'">Exam1 (Центр независимой оценки результатов обучения)</a>',
+        "en": '<a class="nav-link" href="'+exam1+'">Exam1 (Center for Independent Assessment of Learning Outcomes)</a>',
+        "ch": '<a class="nav-link" href="'+exam1+'">Exam1</a>',
+        "ar": '<a class="nav-link" href="'+exam1+'">Exam1</a>',
+    },
+    "link_exam2":{ //футер
+        "ru": '<a class="nav-link" href="'+exam2+'">Exam2</a>',
+        "en": '<a class="nav-link" href="'+exam2+'">Exam2</a>',
+        "ch": '<a class="nav-link" href="'+exam2+'">Exam2</a>',
+        "ar": '<a class="nav-link" href="'+exam2+'">Exam2</a>',
+    },
+    "h1_footer":{ //футер
+        "ru": 'Полезные ссылки',
+        "en": 'Useful links',
+        "ch": '有用的網站鏈接',
+        "ar": 'روابط مفيدة للمواقع',
+    },
+    "h2_footer":{ //футер
+        "ru": 'Учебные платформы:',
+        "en": 'Educational platforms:',
+        "ch": '培訓平台：',
+        "ar": 'منصات التدريب:',
+    },
+    "footer_images":{ //футер
+        "ru": 'Изображения:',
+        "en": 'Images:',
+        "ch": '圖片：',
+        "ar": 'الصور:',
+    },
+    "footer_about_us":{ //футер
+        "ru": 'О нас:',
+        "en": 'About Us:',
+        "ch": '關於我們：',
+        "ar": 'معلومات عنا',
+    },
+    "footer_about_us_p1":{ //футер
+        "ru": 'Расскажем немного про себя. Наша команда «Тюлени Свободы» была создана в 2023 для создания классных программных продуктов',
+        "en": 'Lets tell you a little about ourselves. Our team "Freedom Seals" was created in 2023 to create cool software products',
+        "ch": '讓我們向您介紹一下我們自己。 我們的團隊「Freedom Seals」成立於 2023 年，致力於創造酷炫的軟體產品',
+        "ar": 'دعنا نخبرك قليلاً عن أنفسنا. تم إنشاء فريقنا "Freedom Seals" في عام 2023 لإنشاء منتجات برمجية رائعة',
+    },
+    "footer_about_us_p2":{ //футер
+        "ru": 'Состав команды изменялся, и на данный момент в неё входят:',
+        "en": 'The composition of the team has changed, and currently includes:',
+        "ch": '團隊的組成發生了變化，目前包括：',
+        "ar": 'لقد تغير تشكيل الفريق، وأصبح يضم حاليا:',
+    },
+    "footer_about_us_ul":{ //футер
+        "ru": '<li>Глеб Шилов - тимлид и frontend разработчик</li><li>Кирилл Серебряков - аналитик</li><li>Вероника Гайнулаева - копирайтер и переводчик</li><li>Александр Севрюк - дизайнер</li><li> Дмитрий Китаев - frontend и backend разработчик</li>',
+        "en": '<li>Gleb Shilov - team lead and frontend developer</li><li>Kirill Serebryakov - analyst</li><li>Veronica Gainuleva - copywriter and translator</li><li>Alexander Sevryuk  - designer</li><li> Dmitriy Kitaev - frontend and backend developer</li>',
+        "ch": '<li>Gleb Shilov - 團隊領導與前端開發人員</li><li>Kirill Serebryakov - 分析師</li><li>Veronica Gainuleva - 文案和翻譯</li><li>Alexander Sevryuk  - 設計師</li><li> Dmitriy Kitaev  - 前端和後端開發人員</li>',
+        "ar": '<li>Gleb Shilov - قائد الفريق ومطور الواجهة الأمامية</li><li>Kirill Serebryakov - المحلل</li><li>Veronica Gainuleva - كاتب ومترجم</li><li>Alexander Sevryuk  - مصمم</li><li> Dmitriy Kitaev - مطور الواجهة الأمامية والخلفية</li>',
+    },
+    "to_lesson_1":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_1+'?lang=ru&">К первому уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_1+'?lang=en&">To the first lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_1+'?lang=ch&">進入第一課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_1+'?lang=ar&">إلى الدرس الأول!</a></button>',
+    },
+    "to_lesson_2":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_2+'?lang=ru&">К второму уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_2+'?lang=en&">To the second lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_2+'?lang=ch&">繼續第二課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_2+'?lang=ar&">إلى الدرس الثاني!</a></button>',
+    },
+    "to_lesson_3":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_3+'?lang=ru&">К третьему уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_3+'?lang=en&">To the third lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_3+'?lang=ch&">繼續第三課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_3+'?lang=ar&">إلى الدرس الثالث!</a></button>',
+    },
+    "to_lesson_4":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_4+'?lang=ru&">К четвертому уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_4+'?lang=en&">To the fourth lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_4+'?lang=ch&">繼續第四課</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_4+'?lang=ar&">إلى الدرس الرابع!</a></button>',
+    },
+    "to_lesson_5":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_5+'?lang=ru&">К пятому уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_5+'?lang=en&">To the fifth lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_5+'?lang=ch&">繼續第五課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_5+'?lang=ar&">إلى الدرس الخامس!</a></button>',
+    },
+    "to_lesson_6":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_6+'?lang=ru&">К шестому уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_6+'?lang=en&">To the sixth lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_6+'?lang=ch&"繼續第六課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_6+'?lang=ar&">إلى الدرس السادس!</a></button>',
+    },
+    "to_lesson_7":{ //ссылка на урок
+        "ru": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_7+'?lang=ru&">К седьмому уроку!</a></button>',
+        "en": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_7+'?lang=en&">To the seventh lesson!</a></button>',
+        "ch": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_7+'?lang=ch&"繼續第七課！</a></button>',
+        "ar": '<button type="button" class="btn btn-success"> <a class="white_link" href="'+lesson_7+'?lang=ar&">إلى الدرس السابع!</a></button>',
+    },
+    
+};
