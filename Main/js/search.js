aa0b9890ff574eb6ebb1fbac95c8678b2c4813e1
mitya <140421001+dm3tr-0@users.noreply.github.com>
2024-05-10 19:53:25 +0500

    Update search.js
    теперь правильно создает ссылку(почти всегда правильный айдишник в ссылке^-^)

--- Main/js/search.js
+++ Main/js/search.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Проход по содержимому файлов translations
         for (let i = 1; i <= 7; i++) {
+            let prev = '';
             fetch(`js/translations${i}.js`)
                 .then(response => {
                     if (!response.ok) {
@@ -26,10 +27,27 @@ document.addEventListener("DOMContentLoaded", function () {
                             let lang = '';
                             let anchorId = '';
                             if (match) {
-                                lang = match[1];
-                                anchorId = match[2];
+                                    lang = match[1];
+                                    anchorId = match[2];
                             }
-                            const href = lang && anchorId ? `lesson${i}.html?lang=${lang}&#${anchorId}` : `lesson${i}.html`;
+                            const regex3 = /\id="(\w+)"/g;
+                            const match3 = regex3.exec(line);
+                            let newid = '';
+                            if (match3) {
+                                newid = match2[1];
+                            }
+                            let href = '';
+                            //const href = lang && anchorId ? `lesson${i}.html?lang=${lang}&#${anchorId}` : `lesson${i}.html?lang=ru&#${prev}`;
+                            if (lang && anchorId) {
+                                href = `lesson${i}.html?lang=${lang}&#${anchorId}`;
+                            }
+                            else if (newid) {
+                                href = `lesson${i}.html?lang=ru&#${newid}`;
+                            }
+                            else {
+                                href = `lesson${i}.html?lang=ru&#${prev}`;
+                            }
+                            //prev = '';
                             const link = document.createElement("a");
                             link.href = href;
                             link.textContent = `Найдено в lesson${i}.html`;
@@ -38,6 +56,21 @@ document.addEventListener("DOMContentLoaded", function () {
                             listItem.appendChild(link);
                             searchResultsDropdown.appendChild(listItem);
                             linkCreated = true; // Устанавливаем флаг, что ссылка создана
+                        }
+                        if (!line.toLowerCase().includes(searchTerm)) {
+                            const regex2 = /\"(\w+)":{/g;
+                            const match2 = regex2.exec(line);
+                            if (match2) {
+                                prev = match2[1];
+                            }
+                            else {
+                                const regex4 = /\id="(\w+)"/g; 
+                                const match4 = regex4.exec(line);
+                                if (match4) {
+                                    prev = match4[1];
+                                }
+
+                            }
                         }
                     });
                 })
